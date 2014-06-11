16a6b6e9d4c1111ceb39ee19302d20e23816543f
jcbaka <shaltan.jean143@gmail.com>
2014-06-11 08:47:10 -0700

    environment summary change to list of projects (no sub group)

--- application/views/environment/envi_summary.php
+++ application/views/environment/envi_summary.php
@@ -1,53 +1,100 @@
+<div style="width:910px;overflow-x: auto;">
 <table summary="Summary Here" class="summary">
         <thead>
           <tr>
-            <th rowspan="2">SUB-GROUP / IMPLEMENTING AGENCY </th>
+            <th rowspan="2">LOCATION</th>
+            <th rowspan="2">PROJECT DESCRIPTION</th>
+            <th rowspan="2">Implementing Agency</th>
+            <th rowspan="2">INDICATIVE AMOUNT</th>
+            <th rowspan="2">FUNDING STATUS (FULL, UNFUNDED, PARTIAL)</th>
             <th colspan="3">FUNDING SCHEDULE</th>
-            <th rowspan="2">TOTAL PROJECT COST</th>
+            <th colspan="3">FUND SOURCE</th>
+            <th rowspan="2">REMARKS</th>
           </tr>
           <tr>
             <th>2014</th>
             <th>2015</th>
             <th>2016</th>
+            <th>Nat'l. Gov't</th>
+            <th>Local Gov't</th>
+            <th>Others</th>
           </tr>
         </thead>
         <tbody>
-          <tr class="light">
-             <td>Environment</td>
-            <td>3,310,236,385.00</td>
+           <tr class="light">
+            <td rowspan="5" style="vertical-align: middle">Province of Leyte</td>
+            <td>Reforestation of Integrated Social Forestry (ISF) areas in 16 Municipalities</td>
+            <td>Environment and Natural Resources Office (ENRO)</td>
+            <td>180,000,000.00</td>
+            <td>UNFUNDED</td>
+            <td>180,000,000.00</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr class="dark">
+           	<td>Demolition and Clearing of Felled Coconut Trees</td>
+            <td>ENRO</td>
+            <td>2,094,228,785.00</td>
+            <td>UNFUNDED</td>
+            <td>2,094,228,785.00</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+           <tr class="light">
+            <td>Bamboo Plantation</td>
+            <td>ENRO</td>
+            <td>1,000,000.00</td>
+            <td>UNFUNDED</td>
+            <td>1,000,000.00</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr class="dark">
+           	<td>A Sustainable Biomas Cogeneration Powerplant System</td>
+            <td>ENRO</td>
+            <td>2,000,000,000.00</td>
+            <td>UNFUNDED</td>
+            <td>1,000,000,000.00</td>
             <td>1,000,000,000.00</td>
             <td></td>
-            <td>4,310,236,385.00</td>
-          </tr>
-            <!-- <tr class="dark">
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-           <tr class="light">
-             <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr class="dark">
-            <td></td>
             <td></td>
             <td></td>
             <td></td>
             <td></td>
           </tr>
            <tr class="light">
-             <td></td>
+            <td>Debris Management</td>
+            <td>ENRO</td>
+            <td>35,007,600.00</td>
+            <td>UNFUNDED</td>
+            <td>35,007,600.00</td>
+            <td></td>
+            <td></td>
             <td></td>
             <td></td>
             <td></td>
             <td></td>
           </tr>
-          <tr class="dark">
+         <!--  <tr class="dark">
+           <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
             <td></td>
             <td></td>
             <td></td>
@@ -55,7 +102,14 @@
             <td></td>
           </tr>
          <tr class="light">
-             <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
             <td></td>
             <td></td>
             <td></td>
@@ -67,18 +121,31 @@
             <td></td>
             <td></td>
             <td></td>
-          </tr>-->
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr> -->
         </tbody>
         <tfoot>
         	<tr>
-        		<td>SUB TOTAL</td>
-        		<td>3,310,236,385.00</td>
-        		<td>1,000,000,000.00</td>
-        		<td></td>
-        		<td>4,310,236,385.00</td>
+        		<td colspan="3" style="text-align: center">GRAND TOTAL</td>
+	            <td>4,310,236,385.00</td>
+	            <td></td>
+	            <td>3,310,236,385.00</td>
+	            <td>1,000,000,000.00</td>
+	            <td></td>
+	            <td></td>
+	            <td></td>
+	            <td></td>
+	            <td></td>
         	</tr>
         </tfoot>
       </table>
+</div>
 <!-- ################################################################################ -->
 <!-- Summary of Projects -->
 <!-- <table summary="Summary Here" class="summary">
