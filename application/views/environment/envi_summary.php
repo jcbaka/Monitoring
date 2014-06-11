f0d241cfa33c00866c8f3b89abd4a73f2fa68e67
jcbaka <shaltan.jean143@gmail.com>
2014-06-11 08:20:46 -0700

    environment summary change to list of projects (no sub group)

--- application/views/environment/envi_summary.php
+++ application/views/environment/envi_summary.php
@@ -79,3 +79,37 @@
         	</tr>
         </tfoot>
       </table>
+<!-- ################################################################################ -->
+<!-- Summary of Projects -->
+<!-- <table summary="Summary Here" class="summary">
+        <thead>
+          <tr>
+            <th rowspan="2">SUB-GROUP / IMPLEMENTING AGENCY </th>
+            <th colspan="3">FUNDING SCHEDULE</th>
+            <th rowspan="2">TOTAL PROJECT COST</th>
+          </tr>
+          <tr>
+            <th>2014</th>
+            <th>2015</th>
+            <th>2016</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="light">
+             <td>Environment</td>
+            <td>3,310,236,385.00</td>
+            <td>1,000,000,000.00</td>
+            <td></td>
+            <td>4,310,236,385.00</td>
+          </tr>
+        </tbody>
+        <tfoot>
+        	<tr>
+        		<td>SUB TOTAL</td>
+        		<td>3,310,236,385.00</td>
+        		<td>1,000,000,000.00</td>
+        		<td></td>
+        		<td>4,310,236,385.00</td>
+        	</tr>
+        </tfoot>
+      </table> -->
